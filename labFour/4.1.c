382d809dfa24a77cf6a8dd3c3bed5e81a5256696
SARAH ALSHEHRI <46406878+sarahshehri@users.noreply.github.com>
2022-04-03 16:08:22 +0300

    Create 4.1.c

---
+++ labFour/4.1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <iostream>
+/*
+Hello World
+*/
+int main(void) {
+	printf("hello, world\n");
+	//tells the OS to run the pause program. This program waits to be terminated, and halts the exceution of the parent C program
+	//system("pause");
+	return 0;
+}
